b78ed19d61f42a3129e55461fb7511f5ca6f8863
Lucas-Chen-10 <yahnn@163.com>
2022-08-21 13:28:02 +0800

    修改shared_ptr内容

--- shared_ptr_used.cpp
+++ shared_ptr_used.cpp
@@ -61,16 +61,18 @@ int main()
     std::shared_ptr<Test> ptr5; //mismatched types ‘std::unique_ptr<_Tp, _Dp>’ and ‘Test*’
     ptr5 = p;*/
 
-    /*3.循环引用*/
+    /*3.循环引用
+    两个对象互相使用一个 shared_ptr 成员变量指向对方。这样会引发一个问题，其中任何一个对象的引用计数都为2。
+    析构时两个资源引⽤计数会减⼀，于是两者引⽤计数还是大于0，导致跳出函数时资源没有被释放（离开作用域时两个对象的析构函数没有被调⽤）*/
     {
         std::shared_ptr<Test> ptr6(new Test);
         std::shared_ptr<Test2> ptr7(new Test2);
-        std::cout << "ptr6引用计数：" << ptr6.use_count() << std::endl;
-        std::cout << "ptr7引用计数：" << ptr7.use_count() << std::endl;
+        std::cout << "ptr6引用计数：" << ptr6.use_count() << std::endl;  //1
+        std::cout << "ptr7引用计数：" << ptr7.use_count() << std::endl;  //1
         ptr6->ptr = ptr7;
         ptr7->ptr = ptr6;
-        std::cout << "ptr6引用计数：" << ptr6.use_count() << std::endl;
-        std::cout << "ptr7引用计数：" << ptr7.use_count() << std::endl;
+        std::cout << "ptr6引用计数：" << ptr6.use_count() << std::endl;  //2
+        std::cout << "ptr7引用计数：" << ptr7.use_count() << std::endl;  //2
     }
 
     return 0;
